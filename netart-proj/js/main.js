6e2501c8ce45d68e4a3804b15c2c6f31cf08a214
sarah <sarah.desai@sjsu.edu>
2025-10-05 01:41:05 -0700

    added home & breathe pages

---
+++ netart-proj/js/main.js
@@ -0,0 +1,31 @@
+document.querySelector("#popup1").addEventListener("click", function(){
+    document.querySelector("#popup1").style.visibility = "hidden";
+})
+
+document.querySelector("#popup2").addEventListener("click", function(){
+    document.querySelector("#popup2").style.visibility = "hidden";
+})
+
+document.querySelector("#popup3").addEventListener("click", function(){
+    document.querySelector("#popup3").style.visibility = "hidden";
+})
+
+document.querySelector("#popup4").addEventListener("click", function(){
+    document.querySelector("#popup4").style.visibility = "hidden";
+})
+
+document.querySelector("#popup5").addEventListener("click", function(){
+    document.querySelector("#popup5").style.visibility = "hidden";
+})
+
+document.querySelector("#popup6").addEventListener("click", function(){
+    document.querySelector("#popup6").style.visibility = "hidden";
+})
+
+document.querySelector("#popup7").addEventListener("click", function(){
+    document.querySelector("#popup7").style.visibility = "hidden";
+})
+
+document.querySelector("#popup8").addEventListener("click", function(){
+    document.querySelector("#popup8").style.visibility = "hidden";
+})
